9aa62e29a003b06db9370a130544e47f4d7ee662
Vinicius Okamoto <53801461+ViniOkamoto@users.noreply.github.com>
2019-12-28 23:16:24 -0300

    Mongo Port

--- GoBarber/src/database/index.js
+++ GoBarber/src/database/index.js
@@ -23,7 +23,7 @@ class Database {
 
   mongo() {
     this.mongoConnection = mongoose.connect(
-      'mongodb://localhost:27017/gobarber',
+      'mongodb://localhost:27016/gobarber',
       {
         useUnifiedTopology: true,
         useNewUrlParser: true,
