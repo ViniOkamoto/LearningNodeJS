6c574e0a7f4eb581915c23133b9b8a06f2fcbc40
Vinicius Okamoto <53801461+ViniOkamoto@users.noreply.github.com>
2020-01-02 22:33:42 -0300

    Sentry, youch, express async errors

--- GoBarber/src/app.js
+++ GoBarber/src/app.js
@@ -5,23 +5,35 @@
  */
 import express from 'express';
 import path from 'path';
+import Youch from 'youch';
+import * as Sentry from '@sentry/node';
+import 'express-async-errors';
+
 import routes from './routes';
+import sentryConfig from './config/sentry';
+// It's help us with the async errors while we use the sentry
 
 import './database';
 
 class App {
   constructor() {
     this.server = express();
+    Sentry.init(sentryConfig);
+
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
+    // This serves for that we can catch the errors by sentry
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
     /**
      * It serves to direct static files such as images, css and html files that
      * can be accessed directly by the browser.
      */
+
     this.server.use(
       '/files',
       express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
@@ -30,6 +42,19 @@ class App {
 
   routes() {
     this.server.use(routes);
+    // This serves to return the error to sentry.
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    // When we put 4 parameters express understands that middleware is an exception handling
+
+    this.server.use(async (err, req, res, next) => {
+      // Youch help us with errors.
+      const errors = await new Youch(err, req).toJSON();
+
+      return res.status(500).error(errors);
+    });
   }
 }
 
---
+++ GoBarber/src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://f6798acc7a414f66a056c39b949c48a5@sentry.io/1870336',
+};
